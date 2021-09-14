dc50a6bd44490bcdc66e253214e85fe5681877d9
allan galdino <galdinoallancodes@gmail.com>
2021-09-14 18:08:15 -0300

    added gesture recognizer to drag and drop from collection view into view

--- mojiArt/EmojiArtViewController.swift
+++ mojiArt/EmojiArtViewController.swift
@@ -15,6 +15,8 @@ class EmojiArtViewController: UIViewController {
 
     var emojiArtView = EmojiArtView()
 
+    var emojis = "😀🎁✈️🎱🍎🐶🐝🎼🚲♣️🧑🏽‍🎓✏️🌈🤡🎓👻☎️".map { String($0) }
+
     // MARK: - IBOutlets
 
     @IBOutlet weak var dropZone: UIView! {
@@ -34,6 +36,16 @@ class EmojiArtViewController: UIViewController {
 
     @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
     @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+
+    
+    @IBOutlet weak var emojiCollectionView: UICollectionView! {
+        didSet {
+            emojiCollectionView.dataSource = self
+            emojiCollectionView.delegate = self
+            emojiCollectionView.dragDelegate = self
+            emojiCollectionView.dropDelegate = self
+        }
+    }
 }
 
 // MARK: - UIDropInteractionDelegate
@@ -100,6 +112,120 @@ extension EmojiArtViewController: UIScrollViewDelegate {
                size.height > 0 {
                 scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width,
                                             dropZone.bounds.size.height / size.height)
+            }
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension EmojiArtViewController: UICollectionViewDataSource,
+                                  UICollectionViewDelegate,
+                                  UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return emojis.count
+    }
+
+    private var font: UIFont {
+        UIFontMetrics(forTextStyle: .body)
+            .scaledFont(for: UIFont
+                            .preferredFont(forTextStyle: .body)
+                            .withSize(48.0))
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView
+                .dequeueReusableCell(withReuseIdentifier: "EmojiCell",
+                                     for: indexPath) as? EmojiCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let text = NSAttributedString(string: emojis[indexPath.item], attributes: [.font: font])
+        cell.label.attributedText = text
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDragDelegate
+
+extension EmojiArtViewController: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        itemsForBeginning session: UIDragSession,
+                        at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return dragItems(at: indexPath)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        itemsForAddingTo session: UIDragSession,
+                        at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        return dragItems(at: indexPath)
+    }
+
+    private func dragItems(at indexPath: IndexPath) -> [UIDragItem]{
+        if let attributedString = (emojiCollectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.label.attributedText {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: attributedString))
+            dragItem.localObject = attributedString
+            return [dragItem]
+        }
+        return []
+    }
+}
+
+// MARK: - UICollectionViewDropDelegate
+
+extension EmojiArtViewController: UICollectionViewDropDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        dropSessionDidUpdate session: UIDropSession,
+        withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return UICollectionViewDropProposal(operation: isSelf ? .move : .copy,
+                                            intent: .insertAtDestinationIndexPath)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0,
+                                                                                 section: 0)
+        for item in coordinator.items {
+            if let sourceIndexPath = item.sourceIndexPath {
+                if let attributedString = item.dragItem.localObject as? NSAttributedString {
+                    collectionView.performBatchUpdates {
+                        emojis.remove(at: sourceIndexPath.item)
+                        emojis.insert(attributedString.string, at: destinationIndexPath.item)
+                        collectionView.deleteItems(at: [sourceIndexPath])
+                        collectionView.insertItems(at: [destinationIndexPath])
+                    }
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                }
+            } else {
+                let placeholderContext = coordinator.drop(
+                    item.dragItem,
+                    to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath,
+                                                        reuseIdentifier: "DropPlaceholderCell"))
+
+                item.dragItem
+                    .itemProvider
+                    .loadObject(ofClass: NSAttributedString.self) { (provider, error) in
+                        DispatchQueue.main.async {
+                            if let attributedString = provider as? NSAttributedString {
+                                placeholderContext.commitInsertion { insertionIndexPath in
+                                    self.emojis.insert(attributedString.string,
+                                                       at: insertionIndexPath.item)
+                                }
+                            } else {
+                                placeholderContext.deletePlaceholder()
+                            }
+                        }
+                }
             }
         }
     }
---
+++ mojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiCollectionViewCell.swift
+//  mojiArt
+//
+//  Created by allan galdino on 14/09/21.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
